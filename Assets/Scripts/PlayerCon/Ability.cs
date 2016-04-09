9211c5f04e240db5138d12faa8c5c9ec08f227b0
Alex Boyle <aboyle75@gmail.com>
2016-04-09 14:34:04 -0500

    small change to Ability

--- Assets/Scripts/PlayerCon/Ability.cs
+++ Assets/Scripts/PlayerCon/Ability.cs
@@ -2,6 +2,6 @@
 using System.Collections;
 using XInputDotNetPure;
 public abstract class Ability{
-	public int abilityState = 0;
+
 	public abstract void abilityUpdate(GamePadState state,GamePadState prestate);
 }
